b39982fe49fe4ad4c3c2016b8c11773c1565c16e
yongpark <yongpark.sd@gmail.com>
2017-05-19 17:32:52 -0700

    calls work

--- make_call.js
+++ make_call.js
@@ -1,15 +1,13 @@
-var twilio = require('twilio');
-
 // Download the Node helper library from twilio.com/docs/node/install
 // These vars are your accountSid and authToken from twilio.com/user/account
 var accountSid = 'AC2a030dd4c2eefc7ace3f2f9c63495c74';
-var authToken = "your_auth_token";
+var authToken = "5d5a3358a582439bac75aa3ac3c97e95";
 var client = require('twilio')(accountSid, authToken);
 
 client.calls.create({
     url: "http://demo.twilio.com/docs/voice.xml",
-    to: "+14155551212",
-    from: "+15017250604"
+    to: "+16193180160",
+    from: "+14152125938 "
 }, function(err, call) {
     process.stdout.write(call.sid);
 });
