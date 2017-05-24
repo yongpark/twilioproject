c77244a7ac44a7cf7264afbd2cdc9ba02da3e750
yongpark <yongpark.sd@gmail.com>
2017-05-23 19:27:22 -0700

    resets git

--- components/twilio.jsx
+++ components/twilio.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
-import express from 'express';
 
 import styles from '../assets/styles.scss';
 import coolStyle from '../assets/test_file.css';
@@ -21,46 +20,39 @@ class Twilio extends React.Component {
       message: '',
       date: null,
     };
+    this.updateDate = this.updateDate.bind(this);
+    this.updateNumber = this.updateNumber.bind(this);
+    this.updateTime = this.updateTime.bind(this);
+  }
+  updateNumber(){
+    return e => {
+      const number = e.target.value;
+      this.setState({phoneNumber: number});
+      console.log(this.state);
+    };
   }
 
-  buildResponse(){
-    const VoiceResponse = require('twilio').twiml.VoiceResponse;
-    const urlencoded = require('body-parser').urlencoded;
+  updateDate(){
+    return e => {
+      const date = new Date(e.target.value);
+      this.setState({date: date});
+      console.log(this.state);
+    };
+  }
 
-    const app = express();
-
-    // Parse incoming POST params with Express middleware
-    app.use(urlencoded({extended: false}));
-
-    // Create a route that will handle Twilio webhook requests, sent as an
-    // HTTP POST to /voice in our application
-    app.post('/voice', (request, response) => {
-      // Get information about the incoming call, like the city associated
-      // with the phone number (if Twilio can discover it)
-      const city = request.body.FromCity;
-
-      // Use the Twilio Node.js SDK to build an XML response
-      //edit for user input
-      const twiml = new VoiceResponse();
-      twiml.say({voice: 'alice'},
-        `Never gonna give you up ${city}.`
-      );
-      twiml.play({}, 'https://demo.twilio.com/docs/classic.mp3');
-
-      // Render the response as XML in reply to the webhook request
-      response.type('text/xml');
-      response.send(twiml.toString());
-    });
-
-    // Create an HTTP server and listen for requests on port 3000
-    app.listen(3000);
+  updateTime(){
+    return e => {
+      const time = e.target.value;
+      this.setState({time: time});
+      console.log(this.state);
+    };
   }
 
   twilioCall(){
+    //TwilioAPICall
     const accountSid = 'AC2a030dd4c2eefc7ace3f2f9c63495c74';
     const authToken = '5d5a3358a582439bac75aa3ac3c97e95';
     const client = require('twilio')(accountSid, authToken);
-    //TwilioAPICall
     client.calls
       .create({
         url: 'http://demo.twilio.com/docs/voice.xml',
@@ -69,8 +61,6 @@ class Twilio extends React.Component {
       })
       .then((call) => process.stdout.write(call.sid));
   }
-
-
 
 
   render(){
