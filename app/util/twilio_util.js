317d278faa5ffe27fcccc24d31d74aefb7a3cff1
yongpark <yongpark.sd@gmail.com>
2017-05-29 23:17:26 -0700

    starts configuring twilio api call

--- app/util/twilio_util.js
+++ app/util/twilio_util.js
@@ -15,3 +15,17 @@ export const createCall = call => (
     data: {phoneNumber: call.phoneNumber, message: call.message, date: call.date}
   }).then(() => console.log(call))
 );
+
+const accountSid = 'AC2a030dd4c2eefc7ace3f2f9c63495c74';
+const authToken = '5d5a3358a582439bac75aa3ac3c97e95';
+const client = require('twilio')(accountSid, authToken);
+
+export const makeCall() => (
+  client.calls.
+  .create({
+    url: 'http://demo.twilio.com/docs/voice.xmlhttps://handler.twilio.com/twiml/EH6b4fda028dda8f39e767fded83c6ff87?Message=',
+    to: '+16193180160',
+    from: '+14152125938',
+  })
+  .then((call) => process.stdout.write(call.sid));
+)
